08490e5a41ee935332a81c59c88055db43a9c6a6
Cha0s-L <31586256+Cha0s-L@users.noreply.github.com>
2024-04-24 15:18:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const moment = require('moment-timezone');
 const urls = [
 	'https://argox-q78213664.koyeb.app/',             // 此处可备注名称，例如：glitch
 	'https://traveling-night-epoxy.glitch.me' ,
+	'https://61ac657e-fd7e-4683-a5b7-e89dcdf66e2d-00-spkxmb04phx7.kirk.replit.dev/' ,
 	'https://test-keep.onrender.com'// 此处可备注名称，例如：Back4app
 
 ];
